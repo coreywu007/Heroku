371be6ea0751146cd53f88e2461ec6a8d454976c
Corey Wu <coreywu@nwk--187-211.nwk.ruw.rutgers.edu>
2017-04-22 16:27:54 -0400

    another commit

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
+app.get('/byeworld', function (req, res) {
+  res.send('Bye')
+})
+
 app.listen(PORT, function(){
 	console.log('Running')
 });
